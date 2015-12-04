b6db570f20cc68cac349d69febe8ac7b138cbf45
ivanfoo <sanfru.ivan@gmail.com>
2015-12-04 03:28:15 +0100

    Night playing

---
+++ gossip.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ivanfoo/gossip/bot"
+)
+
+const (
+	VERSION = "0.1"
+)
+
+func usage() {
+	fmt.Printf(	
+		`	
+Usage:
+    gossip -s slackBotToken [-u user] [-i identity_file]
+
+where:
+    -s slackBotToken is the API token for the Slack bot
+    -u user
+    -i identity_file
+`, VERSION)
+	os.Exit(1)
+}
+
+func main() {
+	var SlackToken = flag.String("s", "", "create Slack bot")
+	var Username = flag.String("u", "", "ssh user to use")
+	var SSHKeyPath = flag.String("i", "", "private key to use")
+
+	flag.Parse()
+
+	if *SlackToken == "" {
+		usage()
+	}
+
+	bot := bot.NewBot(bot.BotOptions{
+		Username:   *Username,
+		SSHKeyPath: *SSHKeyPath,
+		SlackToken: *SlackToken,
+	})
+
+	bot.Run()
+}
